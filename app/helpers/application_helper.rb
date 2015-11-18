8ca16a8175574013d3ee952cfe01533d9c64b597
diana <diana.carvalho@tutanota.com>
2015-11-18 19:41:20 +0000

    devise and activeadmin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,11 @@ module ApplicationHelper
 	    country = ISO3166::Country[country_code]
 	    country.translations[I18n.locale.to_s] || country.name
   	end
+	def controller?(*controller)
+	    controller.include?(params[:controller])
+	end
+
+	def action?(*action)
+	    action.include?(params[:action])
+	end
 end
