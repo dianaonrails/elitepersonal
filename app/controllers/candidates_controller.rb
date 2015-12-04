e1b4254c82732eff8faa10b119ff561989f873f7
diana <diana.carvalho@tutanota.com>
2015-12-04 02:29:35 +0000

    aditional question

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -90,7 +90,7 @@ class CandidatesController < ApplicationController
     def candidate_params
       params.require(:candidate).permit(:photo,:category_id,:first_name,:last_name,:address,:mobile,:email,:citizenship,:birth_date,
         :gender,:height,:weight,:passport,:foreign_passport,:marital_status,:children,:sign,:nationality,
-        :smoker, :car,:driving_licence,education_info_attributes:[:level,:history,:languages],work_info_attributes:[:sectors_experience,:years_experience,
+        :smoker, :car,:driving_licence,education_info_attributes:[:level,:history,:languages],:description,work_info_attributes:[:sectors_experience,:years_experience,
           :current_job,:last_employer,:key_skills,:salary],:availability_ids => [],:available_work_ids =>[],:legal_work_ids =>[])
     end
 end
