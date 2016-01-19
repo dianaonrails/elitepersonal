9ff7cefc17b69117f5d0a00ac8d9c5ed2478b1f8
diana carvalho <dianacarvalho@dianas-MBP.home>
2016-01-19 23:46:49 +0000

    migration

--- db/migrate/20160115234420_add_birth_to_one_to_nanny_question.rb
+++ db/migrate/20160115234420_add_birth_to_one_to_nanny_question.rb
@@ -1,5 +1,5 @@
 class AddBirthToOneToNannyQuestion < ActiveRecord::Migration
   def change
-    #add_column :nanny_questions, :birth_to_one, :string
+    add_column :nanny_questions, :birth_to_one, :string
   end
 end
