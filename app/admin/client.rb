391276b435dbea86f2758a408a96c7bb5ddf9e92
diana carvalho <dianacarvalho@dianas-MBP.lan>
2016-05-03 23:18:09 +0100

    new client

--- app/admin/client.rb
+++ app/admin/client.rb
@@ -3,7 +3,19 @@ ActiveAdmin.register Client do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-	permit_params :name, :email, :phone, :message
+	permit_params :name, :email, :phone, :message,:category_id
+
+	show do
+		attributes_table_for client do
+			row :name
+			row :email
+			row :phone
+			row 'Category' do
+				Category.find(client.category_id).title
+			end
+			row :message
+		end	
+	end		
 #
 # or
 #
