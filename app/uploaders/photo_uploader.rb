f59559241d84778c8b0eaa055fd24c1b4acbfb4f
diana carvalho <dianacarvalho@dianas-MBP.lan>
2016-05-09 00:44:24 +0100

    carrierwaver

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -5,9 +5,9 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
     #include CarrierWave::RMagick
     include CarrierWave::MiniMagick
-
+    
   # Choose what kind of storage to use for this uploader:
-  storage :fog
+  storage :file
   # storage :fog
   process resize_to_fit: [800, 800]
 
