a54b647b22127eae2523bc6fd441a8f884e85f0e
dianac <d.r.carvalho89@gmail.com>
2016-05-09 23:43:26 +0100

    correction

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,11 +3,8 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     'AKIAJQYSAMG67OT35KFA',                        # required
     aws_secret_access_key: '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',                        # required
-    region:                'us-west-2',                  # optional, defaults to 'us-east-1'
-    host:                  's3-website-us-west-2.amazonaws.com',             # optional, defaults to nil
-    endpoint:              'elitepersonalworld.s3-website-us-west-2.amazonaws.com' # optional, defaults to nil
+    region:                'us-west-2'                  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'elitepersonalworld'                          # required
-  config.fog_public     = false                                        # optional, defaults to true
-  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+  
 end
