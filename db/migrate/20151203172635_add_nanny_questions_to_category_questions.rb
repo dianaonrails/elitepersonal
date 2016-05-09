0f3db8e108fc4f2f637d90b4d6405d25f427dbea
dianac <d.r.carvalho89@gmail.com>
2016-05-10 00:03:17 +0100

    migrations

--- db/migrate/20151203172635_add_nanny_questions_to_category_questions.rb
+++ db/migrate/20151203172635_add_nanny_questions_to_category_questions.rb
@@ -1,11 +1,11 @@
 class AddNannyQuestionsToCategoryQuestions < ActiveRecord::Migration
   def change
-  	add_column :category_questions, :cooking, :boolean
-  	add_column :category_questions, :regime, :boolean
-  	add_column :category_questions, :cleaning, :boolean
-  	add_column :category_questions, :education, :boolean
-  	add_column :category_questions, :trips, :boolean
-  	add_column :category_questions, :work_several, :boolean
-  	add_column :category_questions, :household_help, :boolean
+  	#add_column :category_questions, :cooking, :boolean
+  	#add_column :category_questions, :regime, :boolean
+  	#add_column :category_questions, :cleaning, :boolean
+  	#add_column :category_questions, :education, :boolean
+  	#add_column :category_questions, :trips, :boolean
+  	#add_column :category_questions, :work_several, :boolean
+  	#add_column :category_questions, :household_help, :boolean
   end
 end
