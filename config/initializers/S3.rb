233935c8c8f45e69e73d31047b29eac41997f4f1
dianac <d.r.carvalho89@gmail.com>
2016-05-21 15:47:12 +0100

    delete s3 file

--- config/initializers/S3.rb
+++
@@ -1,8 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-      :provider               => 'AWS',
-      
-      :region                 => 'eu-west-1' # Change this for different AWS region. Default is 'us-east-1'
-  }
-  config.fog_directory  = "elitepersonalworldapp"
-end
