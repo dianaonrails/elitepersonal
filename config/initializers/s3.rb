447101a613d02fc1aecee9447d88ccf068934a68
dianac <d.r.carvalho89@gmail.com>
2016-05-23 00:21:59 +0100

    change fog

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -4,9 +4,7 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     Rails.application.secrets.aws_access_key_id,                        # required
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,                        # required
-    region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
-    host:                  's3-website-eu-west-1.amazonaws.com',             # optional, defaults to nil
-    endpoint:              'https://elitepersonalworldapp.s3-website-eu-west-1.amazonaws.com' # optional, defaults to nil
+    region:                'eu-west-1'                  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'elitepersonalworldapp'                          # required
   config.fog_public     = false                                        # optional, defaults to true
