500a520d2e44bc89ff93e5fb1283924b7f05ff29
dianac <d.r.carvalho89@gmail.com>
2016-05-26 14:53:49 +0100

    s3 test

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     Rails.application.secrets.aws_access_key_id,                        # required
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,                        # required
+    aws_access_key_id:     Rails.application.secrets.http_aws_access_key_id,                        # required
+    aws_secret_access_key: Rails.application.secrets.http_aws_secret_access_key,                        # required
     region:                'eu-west-1'                  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'elitepersonalworldapp'                          # required
