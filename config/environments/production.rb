e3feee0536489fe8f9e4d385145d1b0754b6a29d
dianac <d.r.carvalho89@gmail.com>
2016-06-05 15:35:39 +0100

    config production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,5 +94,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.assets.precompile += ['*.js', '*.css', '*.css.erb', '*.css.scss']
+  config.assets.precompile += ['*.js', '*.css', '*.css.erb', '*.css.scss','active_admin.scss']
 end
