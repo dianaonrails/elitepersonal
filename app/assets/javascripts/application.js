560c208ae3b78c7effca9238a59d69bc5321ba11
dianac <d.r.carvalho89@gmail.com>
2016-07-14 23:24:26 +0100

    change javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -72,113 +72,114 @@ function exportPDF(){
     });
     doc.save('candidate-file.pdf');
 };
+(function($) {
+	$(document).ready(function () {
 
-$(document).ready(function () {
-
-	$('.carousel').carousel();
-	$('.datepicker').datepicker();
-	
-
-	$('#exportXLS').click(function(){
-		$("#candidate").table2excel({
-    		name: "Worksheet Candidate",
-    		filename: "Candidate",
-    		fileext: ".xls",
-    		exclude_links: true,
-			exclude_inputs: true
-  		});
-
-  		var htmltable= document.getElementById('#exportXLS');
-        var html = htmltable.outerHTML;
-        window.open('data:application/vnd.ms-excel,' + encodeURIComponent(html));
-	});
-
-	//$('#interview_interview_date').datepicker();
-
-	$("#candidate_mobile").intlTelInput({
-		initialCountry: "ru",
-		preferredCountries: [ "ru", "en" ]
-	});
-
-	I18n.defaultLocale = "<%= I18n.default_locale %>";
-	I18n.locale = "<%= I18n.locale %>";
-
-
-
-
-	var specialElementHandlers = {
-        'body': function (element,renderer) {
-            return true;
-        }
-    };
-
-	
-     
-
-    CategoryQuestions();
-    
-	jQuery('.flyers .flyer-links a').on('click', function(e)  {
-        var currentAttrValue = jQuery(this).attr('href');
- 
-        // Show/Hide Tabs
-        jQuery('.flyers ' + currentAttrValue).show().siblings().hide();
- 
-        // Change/remove current tab to active
-        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
- 
-        e.preventDefault();
-    });
-
-
-	$('.categories-list ul>li a').on('click', function(e){
-		e.preventDefault();
-		$('#vacancies-container').html("");
-		var category = $(this).attr('id');
+		$('.carousel').carousel();
+		$('.datepicker').datepicker();
 		
-		$.ajax({
-          type: "GET",
-          dataType: 'html',
-	      url:'/vacancies/update_category',
-          data: {'category': category},
-          success: function(data) {
-          		if(data){
-          			$('#vacancies-container').append(data);
-          		}
-          		else{
-          			$('#vacancies-container').append('<h3>No records </h3>');
-          		}
-          },
-          error: function(data){
-		    	$('#vacancies-container').append('<h3>No records </h3>');
-          }
-		    
-		  
-        });
-	});
 
-	$("#calendar").fullCalendar({
-	    header: {
-	      left: 'prev,next today',
-	      center: 'title',
-	      right: 'month,agendaWeek,agendaDay'
-	    },
+		$('#exportXLS').click(function(){
+			$("#candidate").table2excel({
+	    		name: "Worksheet Candidate",
+	    		filename: "Candidate",
+	    		fileext: ".xls",
+	    		exclude_links: true,
+				exclude_inputs: true
+	  		});
+
+	  		var htmltable= document.getElementById('#exportXLS');
+	        var html = htmltable.outerHTML;
+	        window.open('data:application/vnd.ms-excel,' + encodeURIComponent(html));
+		});
+
+		//$('#interview_interview_date').datepicker();
+
+		$("#candidate_mobile").intlTelInput({
+			initialCountry: "ru",
+			preferredCountries: [ "ru", "en" ]
+		});
+
+		I18n.defaultLocale = "<%= I18n.default_locale %>";
+		I18n.locale = "<%= I18n.locale %>";
+
+
+
+
+		var specialElementHandlers = {
+	        'body': function (element,renderer) {
+	            return true;
+	        }
+	    };
+
+		
 	     
-	     defaultView: "month",
-	     slotMinutes: 15,
-	     events: "/dashboard/get_events",
-	     timeFormat: "h:mm t{ - h:mm t} ",
-	     dragOpacity: "0.5"
-  	});
 
-	$('#exportPDF').click(function () {
- 		window.print();
- 		return false;
- 		
- 		//var content = document.getElementById('main_content');
+	    CategoryQuestions();
+	    
+		jQuery('.flyers .flyer-links a').on('click', function(e)  {
+	        var currentAttrValue = jQuery(this).attr('href');
+	 
+	        // Show/Hide Tabs
+	        jQuery('.flyers ' + currentAttrValue).show().siblings().hide();
+	 
+	        // Change/remove current tab to active
+	        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+	 
+	        e.preventDefault();
+	    });
 
- 		//var converted = htmlDocx.asBlob(content.innerHTML, {orientation: 'landscape', margins: {top: 100}});
-		//saveAs(converted, 'candidate.docx');
- 		
-    });
 
-});
+		$('.categories-list ul>li a').on('click', function(e){
+			e.preventDefault();
+			$('#vacancies-container').html("");
+			var category = $(this).attr('id');
+			
+			$.ajax({
+	          type: "GET",
+	          dataType: 'html',
+		      url:'/vacancies/update_category',
+	          data: {'category': category},
+	          success: function(data) {
+	          		if(data){
+	          			$('#vacancies-container').append(data);
+	          		}
+	          		else{
+	          			$('#vacancies-container').append('<h3>No records </h3>');
+	          		}
+	          },
+	          error: function(data){
+			    	$('#vacancies-container').append('<h3>No records </h3>');
+	          }
+			    
+			  
+	        });
+		});
+
+		$("#calendar").fullCalendar({
+		    header: {
+		      left: 'prev,next today',
+		      center: 'title',
+		      right: 'month,agendaWeek,agendaDay'
+		    },
+		     
+		     defaultView: "month",
+		     slotMinutes: 15,
+		     events: "/dashboard/get_events",
+		     timeFormat: "h:mm t{ - h:mm t} ",
+		     dragOpacity: "0.5"
+	  	});
+
+		$('#exportPDF').click(function () {
+	 		window.print();
+	 		return false;
+	 		
+	 		//var content = document.getElementById('main_content');
+
+	 		//var converted = htmlDocx.asBlob(content.innerHTML, {orientation: 'landscape', margins: {top: 100}});
+			//saveAs(converted, 'candidate.docx');
+	 		
+	    });
+
+	});
+})(jQuery);
